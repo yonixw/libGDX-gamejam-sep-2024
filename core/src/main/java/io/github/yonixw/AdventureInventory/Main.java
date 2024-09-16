33c51b7e9c74f2d627fd131a70dbfa2a065edc6c
yoniwas <github-dev@yoniwas.com>
2024-09-16 12:30:57 +0300

    font works

--- core/src/main/java/io/github/yonixw/AdventureInventory/Main.java
+++ core/src/main/java/io/github/yonixw/AdventureInventory/Main.java
@@ -3,14 +3,15 @@ package io.github.yonixw.AdventureInventory;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Colors;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -31,26 +32,39 @@ public class Main extends InputAdapter implements ApplicationListener {
     private TextureRegion[][] systemRegions; // [Y][X]
     private Stage stage;
 
-    BitmapFont font;
+    final private Color _black = Colors.get("BLACK");
+    final private Color _white = Colors.get("WHITE");
+
+    BitmapFont fontEBGaramondSemiBold;
+
+    public static final float WIDTH = 720 * 16 / 9, HEIGHT = 720; //or any other values you need
 
     @Override
     public void create() {
         systemTexture = new Texture("TileSet1.png");
         systemRegions = TextureRegion.split(systemTexture, 16, 16);
-        stage = new Stage(new FitViewport(720 * 16 / 9, 720));
+        fontEBGaramondSemiBold = getFont("fonts/EBGaramond-SemiBold.ttf", 25, _white);
+
+        stage = new Stage(new FitViewport(WIDTH, HEIGHT));
         Gdx.input.setInputProcessor(stage);
+
+        Actor txt1 = new MyText(fontEBGaramondSemiBold, "Test Test");
+        txt1.setPosition(0, 100);
+        Actor txt2 = new MyText(fontEBGaramondSemiBold, "Test Test 2");
+        txt2.setPosition(0, 200);
+
+        Group g1 = new Group();
+        g1.addActor(txt1);
+        g1.addActor(txt2);
+        g1.setPosition(100, 0);
+
+        stage.addActor(g1);
 
         Actor curser = new Cursor(systemRegions);
         curser.scaleBy(3);
         stage.addActor(curser);
 
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/gazzarelli.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 12;
-        parameter.color = Colors.get("BLACK");
-        font = generator.generateFont(parameter); // font size 12 pixels
-        generator.dispose(); // don't forget to dispose to avoid memory leaks!
-
+        Gdx.graphics.setSystemCursor(SystemCursor.None);
     }
 
     @Override
@@ -58,27 +72,28 @@ public class Main extends InputAdapter implements ApplicationListener {
         float delta = Gdx.graphics.getDeltaTime();
 
         //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        ScreenUtils.clear(0.7f, 0.7f, 1.0f, 1);
+        //.setProjectionMatrix(stage.getViewport().getCamera().combined);
+        ScreenUtils.clear(0f, 0f, 0f, 1);
         stage.act(delta);
         stage.draw();
 
-        SpriteBatch batch = new SpriteBatch();
+    }
 
-        batch.begin();
-        //font.getData().setScale(.2f);
-        Vector2 xy = stage.getViewport().unproject(new Vector2(100, 100));
-        font.draw(batch, "hello", xy.x, xy.y);
-        batch.end();
+    public BitmapFont getFont(String Path, int Size, Color c) {
+        BitmapFont font;
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(Path));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = Size;
+        parameter.color = c;
+        font = generator.generateFont(parameter); // font size 12 pixels
+        generator.dispose(); // don't forget to dispose to avoid memory leaks!
+        return font;
     }
 
     @Override
     public void dispose() {
         stage.dispose();
         systemTexture.dispose();
-    }
-
-    @Override
-    public void resume() {
     }
 
     @Override
@@ -89,6 +104,10 @@ public class Main extends InputAdapter implements ApplicationListener {
 
     @Override
     public void pause() {
+    }
+
+    @Override
+    public void resume() {
     }
 
 }
